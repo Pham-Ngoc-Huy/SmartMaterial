22bf6e6597447f802fe3e583e597463d5624ecac
Huy Pham Ngoc (VN) <Huy.PhamN1@homecredit.vn>
2024-12-25 13:12:47 +0700

    New Updating on 3 dimension Collab

--- smart_material.py
+++ smart_material.py
@@ -1,9 +1,10 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
-class DataInput:
+class Shape_Memory_Effect:
     def __init__(self, YA, YM, Ms, Mf, As, Af, CM, CA, TCRS, TCRF, SL, temp, shiS0, shiT0):
         self.YA = YA
         self.YM = YM
@@ -20,14 +21,15 @@ class DataInput:
         self.shiS0 = shiS0
         self.shiT0 = shiT0
         self.shi_lst = []
+
     def stress_calculated(self):
         global Ta, Tb, Tc
         Ta = 0
         Tb = self.TCRS
         Tc = self.TCRF
-        
+
     def predicted_through_stress(self):
-        global Tb_predicted, Tc_predicted, Td_predicted
+        global Tb_predicted, Tc_predicted, Td_predicted, Taf_predicted, Tkf_predicted, Ta_predicted
         Tb_predicted = np.linspace(Ta, Tb, 20)    
         Tc_predicted = np.linspace(Tb, Tc, 20)
         for i in Tc_predicted:
@@ -35,28 +37,63 @@ class DataInput:
             self.shi_lst.append(shiS)
         self.shi_lst = np.array(self.shi_lst)
         Td_predicted = np.linspace(Tc,Ta,num=20, endpoint=True)
-        return Tb_predicted, Tc_predicted, Td_predicted
+        Taf_predicted = np.linspace(0,0, num=20, endpoint=True)
+        Tkf_predicted = np.linspace(0,0, num=20, endpoint=True)
+        Ta_predicted = np.linspace(0,0, num=20, endpoint=True) 
     
     def predicted_through_strain(self):
         global Sb_predicted, Sc_predicted, Sd_predicted
         Sb_predicted = Tb_predicted/self.YM
         Sc_predicted = (Tc_predicted/self.YM) + self.SL*self.shi_lst
         Sd_predicted = (Td_predicted/self.YM) + self.SL
-        return Sb_predicted, Sc_predicted, Sd_predicted
-    
-    def plot_T_S_curve(self):
-        strain_combined = np.concatenate((Sb_predicted, Sc_predicted, Sd_predicted))
-        stress_combined = np.concatenate((Tb_predicted, Tc_predicted, Td_predicted))
+                
 
-        plt.figure(figsize=(8, 6))
-        plt.plot(strain_combined, stress_combined, label='Stress-Strain Curve', color='blue')
-        plt.xlabel('Strain')
-        plt.ylabel('Stress (MPa)')
-        plt.title('Stress-Strain Relationship')
-        plt.legend()
-        plt.grid(True)
+    def predicted_through_temp(self):
+        global temp_b_predicted, temp_c_predicted, temp_d_predicted, temp_a_f_predicted, temp_k_f_predicted, temp_a_predicted
+        temp_b_predicted = np.linspace(self.temp,self.temp,num=20, endpoint=True)
+        temp_c_predicted = np.linspace(self.temp,self.temp, num=20, endpoint=True)
+        temp_d_predicted = np.linspace(self.temp,self.temp, num=20, endpoint=True)
+        temp_a_f_predicted = np.linspace(self.temp, self.As, num=20, endpoint=True)
+        temp_k_f_predicted = np.linspace(self.As, self.Af, num=20, endpoint=True)
+        temp_a_predicted = np.linspace(self.Af, self.temp, num=20, endpoint=True)
+
+    def predicted_through_strain_by_temp(self): 
+        global strain_a_f_predicted, strain_k_f_predicted, strain_a_predicted
+        # At a'
+        strain_a_f_predicted = np.linspace(self.SL,self.SL, num=20, endpoint=True)
+        # At k'
+        shi_M_ls = []
+        for i in temp_k_f_predicted:
+            shi_M = 1/2*(1 + math.cos((math.pi/(self.Af-self.As))*(i-self.As)))
+            shi_M_ls.append(shi_M)
+        shi_M_ls = np.array(shi_M_ls)
+        strain_k_f_predicted = self.SL*shi_M_ls
+        # At a
+        strain_a_predicted = np.linspace(0,0, num=20, endpoint=True)
+
+    def plot_T_S_Temp_curve(self):
+        strain_combined = np.concatenate((Sb_predicted, Sc_predicted, Sd_predicted, strain_a_f_predicted, strain_k_f_predicted, strain_a_predicted))
+        stress_combined = np.concatenate((Tb_predicted, Tc_predicted, Td_predicted, Taf_predicted, Tkf_predicted, Ta_predicted))
+        temp_combined = np.concatenate((temp_b_predicted, temp_c_predicted, temp_d_predicted, temp_a_f_predicted, temp_k_f_predicted, temp_a_predicted))
+        # Create a 3D plot
+        fig = plt.figure(figsize=(10, 8))
+        ax = fig.add_subplot(111, projection='3d')
+
+        # Plot the 3D curve
+        ax.plot(strain_combined, stress_combined, temp_combined, label='Stress-Strain-Temp Curve', color='blue')
+        
+        # Set axis labels
+        ax.set_xlabel('Strain')
+        ax.set_ylabel('Temperature (°C)')
+        ax.set_zlabel('Stress (MPa)')
+        ax.set_title('Stress-Strain-Temperature Relationship')
+
+        # Add legend and grid
+        ax.legend()
+        ax.grid(True)
+
+        # Show plot
         plt.show()
-    
 def main():
     Mf = 9  # °C
     Ms = 18  # °C
@@ -72,10 +109,12 @@ def main():
     shiS0 = 0
     SL = 0.07
     temp = 5
-    proc = DataInput(YA, YM, Ms, Mf, As, Af, CM, CA, TCRS, TCRF, SL, temp, shiS0, shiT0)
+    proc = Shape_Memory_Effect(YA, YM, Ms, Mf, As, Af, CM, CA, TCRS, TCRF, SL, temp, shiS0, shiT0)
     proc.stress_calculated()
     proc.predicted_through_stress()
     proc.predicted_through_strain()
-    proc.plot_T_S_curve()
+    proc.predicted_through_temp()
+    proc.predicted_through_strain_by_temp()
+    proc.plot_T_S_Temp_curve()
 if __name__ == "__main__":
     main()
